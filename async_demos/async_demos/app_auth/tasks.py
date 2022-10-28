153bc9b29807a63c8d9c0bf5e6814fe03f2cd042
Bistrev Lyubomir <lubo_bistrev@abv.bg>
2022-10-28 15:55:36 +0300

    Added send email config and requirements.txt

--- async_demos/async_demos/app_auth/tasks.py
+++ async_demos/async_demos/app_auth/tasks.py
@@ -1,5 +1,7 @@
 from celery import shared_task
 from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 
 UserModel = get_user_model()
 
@@ -7,3 +9,13 @@ UserModel = get_user_model()
 @shared_task
 def send_successful_registration_email(user_pk):
     user = UserModel.objects.get(pk=user_pk)
+    context = {
+        'name': 'Lyubo',
+    }
+    message = render_to_string('successful_registration.html', context)
+    send_mail(
+        'Hello from us!',
+        message,
+        'doncho@minkov.it',
+        (user.email,),
+    )
--- async_demos/async_demos/settings.py
+++ async_demos/async_demos/settings.py
@@ -131,5 +131,11 @@ CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = ''
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  # ENV VARIABLES
+EMAIL_HOST_USER = ''  # ENV VARIABLES
+EMAIL_HOST_PASSWORD = ''  # ENV VARIABLES
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
